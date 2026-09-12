5137fcd0a91c54ee0f769f1f83e4903a4d31c9d9
FabricioQs <iefc.kevin.quishpe@gmail.com>
2026-09-11 23:16:34 -0500

    TALLER N° 3: Sistema de Animaciones 2D, Animator

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,42 +5,69 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D rd;
     public float moveSpeed = 5f;
     public float jumpForce = 7f;
-
+    private Animator animator;
     private bool isGrounded;
+    private bool facingRight = true;
 
     void Start()
     {
-        rd = GetComponent<Rigidbody2D>(); 
+        rd = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
         // Movimiento Horizontal
-        float move = Input.GetAxis("Horizontal");
+        float move = Input.GetAxisRaw("Horizontal");
         rd.velocity = new Vector2(move * moveSpeed, rd.velocity.y);
 
-        // Salto con Input.GetButtonDown("Jump") 
+        // Animación de correr/caminar
+        animator.SetFloat("Speed", Mathf.Abs(move));
+
+        // Cambio de dirección
+        if (move > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if (move < 0 && facingRight)
+        {
+            Flip();
+        }
+
+        // Salto
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
             rd.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            isGrounded = false;
+            animator.SetBool("IsJumping", true);
         }
     }
 
-    // Detectar cuando toca la plataforma
+    // Detección de suelo
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
             isGrounded = true;
+            animator.SetBool("IsJumping", false); // Ya aterrizó
         }
     }
 
-    // Detectar cuando sale de la plataforma 
     void OnCollisionExit2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
             isGrounded = false;
+            animator.SetBool("IsJumping", true); // Empezó a caer o saltó
         }
+    }
+
+    // Voltear el sprite
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
     }
 }
